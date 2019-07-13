74c1ac82ba02e1e0f094ca3b198390b266f246e4
Daniel Ichiyama <daniel.ichiyama@gmail.com>
2019-07-13 13:28:53 -1000

    finished, ready for review

--- largestPrimeFactor.js
+++ largestPrimeFactor.js
@@ -1,6 +1,30 @@
 exports.largestPrimeFactor = function(n) {
   let primeNumber = 0;
-  // do your work here
+  let arrOfFactors = [];
+  let arrOfPrimes = [];
+
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) {
+      arrOfFactors.push(i);
+    }
+  }
+
+  let isPrime = function(n) {
+    for (let i = 2; i < n; i++) {
+      if (n % i === 0) {
+        return false;
+      }
+    }
+    return true;
+  };
+
+  for (let i = 0; i < arrOfFactors.length; i++) {
+    if (isPrime(arrOfFactors[i])) {
+      arrOfPrimes.push(arrOfFactors[i]);
+    }
+  }
+
+  primeNumber = arrOfPrimes[arrOfPrimes.length - 1];
 
   return primeNumber;
 };
