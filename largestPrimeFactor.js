2c732b0bdd26f6395f758848e95c535551ea32be
Daniel Ichiyama <daniel.ichiyama@gmail.com>
2019-07-17 17:26:14 -1000

    combined for loops to remove unnecessary code

--- largestPrimeFactor.js
+++ largestPrimeFactor.js
@@ -1,13 +1,6 @@
 exports.largestPrimeFactor = function(n) {
   let primeNumber = 0;
-  let arrOfFactors = [];
   let arrOfPrimes = [];
-
-  for (let i = 2; i < n; i++) {
-    if (n % i === 0) {
-      arrOfFactors.push(i);
-    }
-  }
 
   let isPrime = function(n) {
     for (let i = 2; i < n; i++) {
@@ -18,9 +11,9 @@ exports.largestPrimeFactor = function(n) {
     return true;
   };
 
-  for (let i = 0; i < arrOfFactors.length; i++) {
-    if (isPrime(arrOfFactors[i])) {
-      arrOfPrimes.push(arrOfFactors[i]);
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0 && isPrime(i)) {
+      arrOfPrimes.push(i);
     }
   }
 
